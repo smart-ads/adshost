9d4d73c641fcfe1ac49beb6324bf58b05d764d73
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-03-30 23:16:39 +0300

    web api self host owin

---
+++ core/host.core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace host.core
+{
+    public class Class1
+    {
+    }
+}
