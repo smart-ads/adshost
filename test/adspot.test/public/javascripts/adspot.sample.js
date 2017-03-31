f7839381c95121fc556ba8a7d8bd57d89a3092f1
ulyanov yuriy <ulyanov.yuriy@live.com>
2017-03-31 22:44:29 +0300

    adspot sample test

---
+++ test/adspot.test/public/javascripts/adspot.sample.js
@@ -0,0 +1,2 @@
+﻿'use strict';
+alert('hello');
--- test/adspot.test/routes/index.js
+++ test/adspot.test/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.sendfile('public/html/adspot.sample.html');
 });
 
 module.exports = router;
